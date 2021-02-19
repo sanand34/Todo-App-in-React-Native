a5d1d681a7e57a475d542f756d49393f4644ebcb
sanand34 <sanchitanand34@gmail.com>
2021-02-20 01:00:38 +0530

    Async feature

--- Main.js
+++ Main.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { View, StyleSheet, ScrollView } from "react-native";
 import Todo from "./Todo.js";
 import { v4 } from "uuid";
@@ -9,6 +9,51 @@ import { actionTypes } from "./reducer";
 function Main() {
   const [input, setInput] = useState();
   const [{ user, todos }, dispatch] = useStateValue();
+  const didMountRef = useRef(false);
+  const array = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  const getDate = () => {
+    let date = new Date();
+    const weekday = date.getDay();
+    let daydate = "am";
+    let currentDate = /[0-9]*:[0-9]*:[0-9]*/g.exec(JSON.stringify(date));
+    let day = /[0-9]*-[0-9]*-[0-9]*/g.exec(JSON.stringify(date));
+    let hr = parseInt(/[0-9][0-9]/g.exec(JSON.stringify(currentDate)));
+
+    let min = parseInt(
+      /[0-9][0-9]/g.exec(
+        JSON.stringify(/:[0-9][0-9]:/g.exec(JSON.stringify(currentDate)))
+      )
+    );
+    let sec = /[0-9][0-9]/g.exec(
+      JSON.stringify(/[0-9][0-9][^:]/g.exec(JSON.stringify(currentDate)))
+    );
+    if (hr > 12) {
+      hr = hr - 12 + 5;
+      min = min + 30;
+      hr = hr + Math.floor(min / 60);
+      min = min % 60;
+      daydate = "pm";
+    } else {
+      hr = hr + 5;
+      min = min + 30;
+      hr = hr + Math.floor(min / 60);
+      min = min % 60;
+    }
+    return `\n${day}\n\n${array[weekday - 1]} ${hr}:${min}:${sec} ${daydate}\n`;
+  };
+  Array.prototype.sortBy = function (p) {
+    return this.slice(0).sort(function (a, b) {
+      return a[p] < b[p] ? 1 : a[p] > b[p] ? -1 : 0;
+    });
+  };
   async function _storeData(value) {
     try {
       await AsyncStorage.setItem("10~Tasks", JSON.stringify(value));
@@ -24,36 +69,51 @@ function Main() {
           type: actionTypes.SET_TODOS,
           todos: JSON.parse(value),
         });
-        try {
-          await AsyncStorage.setItem("10~Tasks", value);
-        } catch (error) {
-          console.log(error);
-        }
 
         console.log(JSON.parse(value));
       } else {
+        const date = getDate();
         const key = v4();
         dispatch({
           type: actionTypes.SET_TODOS,
-          todos: [{ key: key, todo: "Welcome", check: false }],
+          todos: [
+            {
+              key: key,
+              todo: "Welcome",
+              check: false,
+              date: date,
+              due_date: "Today",
+            },
+          ],
         });
-        _storeData([{ key: key, todo: "Welcome", check: false }]);
+        _storeData([
+          {
+            key: key,
+            todo: "Welcome",
+            check: false,
+            date: date,
+            due_date: "Today",
+          },
+        ]);
       }
     } catch (error) {
       console.log(error);
     }
   }
+
   useEffect(() => {
     _retrieveData();
   }, []);
   useEffect(() => {
-    const newinfo = todos.filter((todo) => todo.key !== user);
-    todos && _storeData(newinfo);
-    todos &&
-      dispatch({
-        type: actionTypes.SET_TODOS,
-        todos: newinfo,
-      });
+    if (didMountRef.current) {
+      const newinfo = todos.filter((todo) => todo.key !== user);
+      todos && _storeData(newinfo);
+      todos &&
+        dispatch({
+          type: actionTypes.SET_TODOS,
+          todos: newinfo,
+        });
+    } else didMountRef.current = true;
   }, [user]);
 
   return (
@@ -63,7 +123,7 @@ function Main() {
       </Appbar.Header>
 
       <ScrollView style={styles.body}>
-        {todos?.map((todo) => (
+        {todos.sortBy("due_date").map((todo) => (
           <Todo key={todo.key} todo={todo} />
         ))}
       </ScrollView>
@@ -75,12 +135,32 @@ function Main() {
             setInput(text);
           }}
           onSubmitEditing={() => {
+            const key = v4();
+            const date = getDate();
             dispatch({
               type: actionTypes.SET_TODOS,
-              todos: [...todos, { key: v4(), todo: input, check: false }],
+              todos: [
+                ...todos,
+                {
+                  key: key,
+                  todo: input,
+                  check: false,
+                  date: date,
+                  due_date: "Today",
+                },
+              ],
             });
 
-            _storeData([...todos, { todo: input, check: false }]);
+            _storeData([
+              ...todos,
+              {
+                key: key,
+                todo: input,
+                check: false,
+                date: date,
+                due_date: "Today",
+              },
+            ]);
             setInput("");
           }}
         />
--- Todo.js
+++ Todo.js
@@ -1,51 +1,130 @@
-import React, { useState } from "react";
-import { View, StyleSheet, Text } from "react-native";
-import { Checkbox, Button } from "react-native-paper";
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  StyleSheet,
+  Text,
+  Modal,
+  TextInput,
+  Dimensions,
+  Button,
+  Alert,
+  TouchableOpacity,
+} from "react-native";
+import { Checkbox } from "react-native-paper";
 import { useStateValue } from "./StateProvider";
 import { actionTypes } from "./reducer";
-import { _storeData } from "./Main";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+const date = /[0-9]*-[0-9]*-[0-9]*/g.exec(JSON.stringify(new Date()));
+const { width } = Dimensions.get("window");
 const Todo = ({ todo }) => {
-  const [{ todos, user }, dispatch] = useStateValue();
-  const [checked, setChecked] = useState(todo.check);
+  const [{ todos }, dispatch] = useStateValue();
+
+  // This is to manage Modal State
+  const [isModalVisible, setModalVisible] = useState(false);
+
+  // This is to manage TextInput State
+  const [inputValue, setInputValue] = useState("");
+  const [_inputValue, set_InputValue] = useState(date[0]);
+
+  // Create toggleModalVisibility function that will
+  // Open and close modal upon button clicks.
+  const toggleModalVisibility = () => {
+    setModalVisible(!isModalVisible);
+  };
+  const viewData = () =>
+    Alert.alert(
+      todo.todo,
+      todo.date,
+      [{ text: "OK", onPress: () => console.log("OK Pressed") }],
+      { cancelable: false }
+    );
+
+  async function _storeData(value) {
+    try {
+      await AsyncStorage.setItem("10~Tasks", JSON.stringify(value));
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(() => {
+    const newinfo = todos.filter((thistodo) => thistodo.key !== todo.key);
+    if (date[0] == todo.due_date) {
+      _storeData([
+        ...newinfo,
+        {
+          key: todo.key,
+          todo: todo.todo,
+          check: todo.check,
+          date: todo.date,
+          due_date: "Today",
+        },
+      ]);
+      dispatch({
+        type: actionTypes.SET_TODOS,
+        todos: [
+          ...newinfo,
+          {
+            key: todo.key,
+            todo: todo.todo,
+            check: todo.check,
+            date: todo.date,
+            due_date: "Today",
+          },
+        ],
+      });
+    }
+  }, [todo.due_date]);
 
   return (
     <View style={styles.container}>
       <Checkbox
-        status={checked ? true : false}
+        status={todo.check ? "checked" : "unchecked"}
         onPress={() => {
-          async function _storeData(value) {
-            try {
-              await AsyncStorage.setItem("10~Tasks", JSON.stringify(value));
-            } catch (error) {
-              console.log(error);
-            }
-          }
           const newinfo = todos.filter((thistodo) => thistodo.key !== todo.key);
-          setChecked(!todo.check);
           _storeData([
-            { key: todo.key, todo: todo.todo, check: !todo.check },
             ...newinfo,
+            {
+              key: todo.key,
+              todo: todo.todo,
+              check: !todo.check,
+              date: todo.date,
+              due_date: todo.due_date,
+            },
           ]);
           dispatch({
             type: actionTypes.SET_TODOS,
             todos: [
-              { key: todo.key, todo: todo.todo, check: !todo.check },
               ...newinfo,
+              {
+                key: todo.key,
+                todo: todo.todo,
+                check: !todo.check,
+                date: todo.date,
+                due_date: todo.due_date,
+              },
             ],
           });
         }}
         color="white"
       />
       <View style={styles.todo}>
-        <Text style={checked ? styles.textinvalid : styles.textvalid}>
-          {todo.todo}
-        </Text>
+        <TouchableOpacity
+          onPress={() => {
+            viewData();
+          }}
+        >
+          <Text style={todo.check ? styles.textinvalid : styles.textvalid}>
+            {todo.todo}
+          </Text>
+        </TouchableOpacity>
+        <Text
+          style={todo.due_date === "Today" ? styles.text1 : styles.text2}
+        >{`Due:${todo.due_date}`}</Text>
       </View>
 
       <Button
-        icon="delete"
-        color="crimson"
+        title="delete"
+        color="rgb(100,73,234)"
         onPress={() => {
           dispatch({
             type: actionTypes.SET_USER,
@@ -55,12 +134,142 @@ const Todo = ({ todo }) => {
       >
         Delete
       </Button>
+      <Button
+        color="rgb(100,73,234)"
+        title="Edit"
+        onPress={toggleModalVisibility}
+      />
+
+      <Modal
+        animationType="slide"
+        transparent
+        visible={isModalVisible}
+        presentationStyle="overFullScreen"
+        onDismiss={toggleModalVisibility}
+      >
+        <View style={styles.viewWrapper}>
+          <View style={styles.modalView}>
+            <TextInput
+              placeholder="Edit Todo..."
+              value={inputValue}
+              style={styles.textInput}
+              onChangeText={(value) => setInputValue(value)}
+              onSubmitEditing={() => {
+                const newinfo = todos.filter(
+                  (thistodo) => thistodo.key !== todo.key
+                );
+                _storeData([
+                  ...newinfo,
+                  {
+                    key: todo.key,
+                    todo: inputValue,
+                    check: todo.check,
+                    date: todo.date,
+                    due_date: todo.due_date,
+                  },
+                ]);
+                dispatch({
+                  type: actionTypes.SET_TODOS,
+                  todos: [
+                    ...newinfo,
+                    {
+                      key: todo.key,
+                      todo: inputValue,
+                      check: todo.check,
+                      date: todo.date,
+                      due_date: todo.due_date,
+                    },
+                  ],
+                });
+                setInputValue("");
+                toggleModalVisibility();
+              }}
+            />
+            <TextInput
+              placeholder="Edit Due Date..."
+              value={_inputValue}
+              style={styles.textInput}
+              onChangeText={(value) => set_InputValue(value)}
+              onSubmitEditing={() => {
+                const newinfo = todos.filter(
+                  (thistodo) => thistodo.key !== todo.key
+                );
+                _storeData([
+                  ...newinfo,
+                  {
+                    key: todo.key,
+                    todo: todo.todo,
+                    check: todo.check,
+                    date: todo.date,
+                    due_date: _inputValue,
+                  },
+                ]);
+                dispatch({
+                  type: actionTypes.SET_TODOS,
+                  todos: [
+                    ...newinfo,
+                    {
+                      key: todo.key,
+                      todo: todo.todo,
+                      check: todo.check,
+                      date: todo.date,
+                      due_date: _inputValue,
+                    },
+                  ],
+                });
+
+                toggleModalVisibility();
+              }}
+            />
+
+            <Button
+              color="rgb(100,73,234)"
+              title="Close"
+              onPress={toggleModalVisibility}
+            />
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 };
 export default Todo;
 
 const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+  },
+  viewWrapper: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.2)",
+  },
+  modalView: {
+    alignItems: "center",
+    justifyContent: "center",
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    elevation: 5,
+    transform: [{ translateX: -(width * 0.4) }, { translateY: -90 }],
+    height: 180,
+    width: width * 0.8,
+    backgroundColor: "#fff",
+    borderRadius: 7,
+  },
+  textInput: {
+    width: "90%",
+    borderRadius: 5,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    borderColor: "rgba(0, 0, 0, 0.2)",
+    borderWidth: 1,
+    marginBottom: 8,
+  },
   container: {
     flex: 1,
     margin: 10,
@@ -72,12 +281,22 @@ const styles = StyleSheet.create({
   },
   textvalid: {
     fontSize: 30,
-    padding: 30,
+
     color: "white",
+  },
+  text1: {
+    fontSize: 25,
+
+    color: "red",
+  },
+  text2: {
+    fontSize: 25,
+
+    color: "lightgreen",
   },
   textinvalid: {
     fontSize: 30,
-    padding: 30,
+
     textDecorationLine: "line-through",
     color: "grey",
   },
